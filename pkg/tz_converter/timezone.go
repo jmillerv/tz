18424232be591cc4a8e6f89a0edfa56cbc84c1a8
Jeremiah <jmiller.v@gmail.com>
2021-03-27 01:21:24 -0600

    master: timezone conversion function

--- pkg/tz_converter/timezone.go
+++ pkg/tz_converter/timezone.go
@@ -1,1 +1,13 @@
 package tz_converter
+
+import "time"
+
+// TimeIn returns a time for a particular time zone based on the IANA name given
+func TimeIn(t time.Time, name string) (time.Time, error) {
+	location, err := time.LoadLocation(name)
+	if err != nil {
+		return time.Time{}, err
+	}
+	t = t.In(location)
+	return t, nil
+}
